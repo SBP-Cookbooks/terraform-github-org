f6ac2fdc5b08fe6a8ef74091060a1e78b876e00a
Stephen Hoekstra <shoekstra@schubergphilis.com>
2017-04-22 23:26:38 +0200

    Add sbp_messageanalyzer repository

--- deploy.sh
+++ deploy.sh
@@ -10,12 +10,10 @@ git config user.name "$GH_USER_NAME"
 echo "Setting git user email"
 git config user.email "$GH_USER_EMAIL"
 
-echo "Adding git upstream remote"
-git remote add upstream "https://$GH_TOKEN@github.com/$GH_REPO.git"
-
+echo "Committing terraform state changes"
 git checkout master
 git add .
-git commit -m "tfstate: $(TZ=Europe/Amsterdam date) [ci skip]"
+git commit -m "tfstate: $(TZ=Europe/Amsterdam date) [skip ci]"
 
 echo "Pushing changes to upstream master"
-git push upstream master
+git push --quiet "https://$GH_TOKEN@github.com/$GH_REPO.git" master
--- sbp_messageanalyzer.tf
+++ sbp_messageanalyzer.tf
@@ -3,3 +3,15 @@ resource "github_repository" "sbp_messageanalyzer" {
   description = "Development repository for the Message Analyzer cookbook - https://supermarket.chef.io/cookbooks/sbp_messageanalyzer"
   has_issues  = true
 }
+
+resource "github_team" "sbp_messageanalyzer_team" {
+  name        = "sbp_messageanalyzer"
+  description = "sbp_messageanalyzer Cookbook Maintainers"
+  privacy     = "closed"
+}
+
+resource "github_team_repository" "sbp_messageanalyzer_repo" {
+  team_id    = "${github_team.sbp_messageanalyzer_team.id}"
+  repository = "sbp_messageanalyzer"
+  permission = "admin"
+}
