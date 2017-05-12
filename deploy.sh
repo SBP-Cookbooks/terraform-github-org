a241bf007a52506d14901a0e1f8f6d048e02c12f
Stephen Hoekstra <shoekstra@schubergphilis.com>
2017-05-12 16:40:22 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,16 +4,19 @@ set -o errexit -o nounset
 
 terraform apply
 
-echo "Setting git user name"
+echo
+echo "Setting git user name..."
 git config user.name "$GH_USER_NAME"
 
-echo "Setting git user email"
+echo "Setting git user email..."
 git config user.email "$GH_USER_EMAIL"
 
-echo "Committing terraform state changes"
-git checkout master
-git add .
+echo
+echo "Committing terraform state changes..."
+git checkout master > /dev/null
+git add . > /dev/null
 git commit -m "tfstate: $(TZ=Europe/Amsterdam date) [skip ci]"
 
-echo "Pushing changes to upstream master"
-git push --quiet "https://$GH_TOKEN@github.com/$GH_REPO.git" master
+echo
+echo "Pushing changes to upstream master..."
+git push --quiet "https://$GH_USER_NAME:$GH_TOKEN@github.com/$GH_REPO.git" master
